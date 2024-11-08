a4368fe7380d1c9b71bfb8dd8bf0e15d4380b81c
cvelascor1 <c.velascor@uniandes.edu.co>
2024-11-08 04:03:31 +0000

    se ajustan parametros estimadores: 130,160 max depth 14,17

--- modelo_de_regresion_MLFLOW.py
+++ modelo_de_regresion_MLFLOW.py
@@ -80,10 +80,10 @@ y_cluster_1 = y[clust_labels == 1]
 experiment = mlflow.set_experiment("Regresion-DosisOptima")
 
 with mlflow.start_run(experiment_id=experiment.experiment_id):
-    n_estimators_0=150
-    n_estimators_1=200
-    max_depth_0=15
-    max_depth_1=20
+    n_estimators_0=130
+    n_estimators_1=160
+    max_depth_0=14
+    max_depth_1=17
 
     model_0 = RandomForestRegressor(n_estimators=n_estimators_0, max_depth=max_depth_0, random_state=42)
     model_0.fit(X_cluster_0, y_cluster_0)
